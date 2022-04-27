6448eec3ecdd1cb72c57ac7627efbb729edc0f43
Izahhoney <2020489338@student.uitm.edu.my>
2022-04-27 08:20:13 +0800

    for

--- for.php
+++ for.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <?php
-    for ($count = 1; <= 10; $count++) {
+    for ($count = 1; $count<= 10; $count++) {
         if ( ($count % 3) == 0) {
             echo "$count is diviible by 3! <br>";
         }
